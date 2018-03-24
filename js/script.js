4359aace6ca33aa8e7bf2940436b1d1e74a512e7
Piotr Madej <piotr.madejkr@gmail.com>
2018-03-24 17:00:57 +0100

    fix script

--- js/script.js
+++ js/script.js
@@ -1,23 +1,22 @@
 $(function() {
-    
+
     function Button(text) {
     this.text = text || 'Hello';
     }
     
     Button.prototype = {
         create: function() {
-            var btn1 = new Button('Hello!');
+            
             var self = this;
             this.$element = $('<button>');
             this.$element.text(this.text);
             this.$element.click(function() {
-                alert(self.text);
-                $('body').append(this.$element);
-                btn1.create();
+                alert(self.text);  
             });
+            this.$element.appendTo($('body'));
         }
     }
-    
-   
+    var btn1 = new Button('Hello!');
+    btn1.create();
     
 });
