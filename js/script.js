fa92359e47efa590dd5c8b6d936f5bcb26921709
Piotr Madej <piotr.madejkr@gmail.com>
2018-03-24 17:02:23 +0100

    onceagain

--- js/script.js
+++ js/script.js
@@ -18,5 +18,27 @@ $(function() {
     }
     var btn1 = new Button('Hello!');
     btn1.create();
-    
-});
+    var btn2 = new Button('this');
+
+    btn2.create();
+    var btn3 = new Button('is');
+
+    btn3.create();
+
+    var btn4 = new Button('"self"');
+
+    btn4.create();
+
+    var btn5 = new Button('method');
+
+    btn5.create();
+
+    var btn6 = new Button('exercise');
+
+    btn6.create();
+
+    var btn7 = new Button('!');
+
+    btn7.create();
+        
+    });
